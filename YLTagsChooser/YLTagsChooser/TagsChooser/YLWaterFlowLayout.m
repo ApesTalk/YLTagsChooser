4e2c7550d1efb68a884d1a0d985512734f41223b
lumin <lumin@able-elec.com>
2018-02-28 12:24:16 +0800

    修复section没有数据时header/footer也不显示的问题

--- YLTagsChooser/YLTagsChooser/TagsChooser/YLWaterFlowLayout.m
+++ YLTagsChooser/YLTagsChooser/TagsChooser/YLWaterFlowLayout.m
@@ -68,70 +68,69 @@
     CGPoint offset = self.collectionView.contentOffset;
     CGRect visibleRect = CGRectMake(0, offset.y, CGRectGetWidth(self.collectionView.frame), CGRectGetHeight(self.collectionView.frame));
     for(NSInteger section = 0; section < _framesArray.count; section++){
+        CGFloat currentSectionHeight = [self contentHeightInSection:section];
+        CGFloat preSectionHeight = 0;
+        if(section > 0){
+            preSectionHeight = [self contentHeightInSection:section - 1];
+        }
+        
         NSArray *currentSectionFrames = _framesArray[section];
-        for(NSInteger row = 0; row < currentSectionFrames.count; row++){
-            CGRect currentFrame = [currentSectionFrames[row] CGRectValue];
-            NSIndexPath *currentIndexPath = [NSIndexPath indexPathForRow:row inSection:section];
-            if(currentFrame.origin.y + currentFrame.size.height >= visibleRect.origin.y &&
-               currentFrame.origin.y <= visibleRect.origin.y + visibleRect.size.height){
-                //when first row appear
-                if(row == 0){
-                    
-                    //section header should show
-                    UICollectionViewLayoutAttributes *headerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-                                                                                                         atIndexPath:currentIndexPath] copy];
-                    CGRect frame = headerAttr.frame;
-                    if(section == 0){
-                        frame.origin.y = self.collectionView.contentInset.top;
-                    }else{
-                        frame.origin.y = [self contentHeightInSection:section - 1];
+        if(currentSectionFrames.count == 0){
+            //section header should show
+            CGFloat y = section == 0 ? self.collectionView.contentInset.top : preSectionHeight;
+            [attributesArray addObject:[self headerAttri:[NSIndexPath indexPathForRow:0 inSection:section] withY:y]];
+
+            //current section footer should show
+            NSIndexPath *footerIndexPath = [NSIndexPath indexPathForRow:0 inSection:section];
+            y = currentSectionHeight - self.footerReferenceSize.height;
+            [attributesArray addObject:[self footerAttri:footerIndexPath withY:y]];
+            
+            if(section > 0 && section + 1 < _framesArray.count &&
+               y + self.footerReferenceSize.height <= visibleRect.origin.y + visibleRect.size.height){
+                //next section header should show
+                [attributesArray addObject:[self headerAttri:[NSIndexPath indexPathForRow:0 inSection:section + 1] withY:y + self.footerReferenceSize.height]];
+            }
+        }else{
+            for(NSInteger row = 0; row < currentSectionFrames.count; row++){
+                CGRect currentFrame = [currentSectionFrames[row] CGRectValue];
+                NSIndexPath *currentIndexPath = [NSIndexPath indexPathForRow:row inSection:section];
+                if(currentFrame.origin.y + currentFrame.size.height >= visibleRect.origin.y &&
+                   currentFrame.origin.y <= visibleRect.origin.y + visibleRect.size.height){
+                    //when first row appear
+                    if(row == 0){
+                        //section header should show
+                        CGFloat y = section == 0 ? self.collectionView.contentInset.top : preSectionHeight;
+                        [attributesArray addObject:[self headerAttri:currentIndexPath withY:y]];
+                        
+                        if(section > 0){
+                            //pre section's footer should show
+                            NSInteger preSection = section - 1;
+                            NSArray *preSectionFrames = _framesArray[preSection];
+                            NSIndexPath *footerIndexPath = [NSIndexPath indexPathForRow:preSectionFrames.count inSection:preSection];
+                            CGFloat y = preSectionHeight - self.footerReferenceSize.height;
+                            [attributesArray addObject:[self footerAttri:footerIndexPath withY:y]];
+                        }
                     }
-                    headerAttr.frame = frame;
-                    [attributesArray addObject:headerAttr];
                     
+                    //cells should show
+                    UICollectionViewLayoutAttributes *cellAttrs = [[self layoutAttributesForItemAtIndexPath:currentIndexPath] copy];
+                    cellAttrs.frame = currentFrame;
+                    [attributesArray addObject:cellAttrs];
                     
-                    if(section > 0){
-                        //pre section's footer should show
-                        NSInteger preSection = section - 1;
-                        NSArray *preSectionFrames = _framesArray[preSection];
-                        NSIndexPath *footerIndexPath = [NSIndexPath indexPathForRow:preSectionFrames.count inSection:preSection];
-                        UICollectionViewLayoutAttributes *footerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionFooter
-                                                                                                             atIndexPath:footerIndexPath] copy];
-                        CGRect fFrame = footerAttr.frame;
-                        fFrame.origin.y = [self contentHeightInSection:preSection] - self.footerReferenceSize.height;
-                        footerAttr.frame = fFrame;
-                        [attributesArray addObject:footerAttr];
-                    }
-                }
-                
-                //cells should show
-                UICollectionViewLayoutAttributes *cellAttrs = [[self layoutAttributesForItemAtIndexPath:currentIndexPath] copy];
-                cellAttrs.frame = currentFrame;
-                [attributesArray addObject:cellAttrs];
-                
-                
-                //when last cell appear
-                if(row == currentSectionFrames.count - 1 &&
-                   currentFrame.origin.y + currentFrame.size.height + self.sectionInset.bottom <= visibleRect.origin.y + visibleRect.size.height){
-                    //current section footer should show
-                    NSIndexPath *footerIndexPath = [NSIndexPath indexPathForRow:currentSectionFrames.count inSection:section];
-                    UICollectionViewLayoutAttributes *footerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionFooter
-                                                                                                         atIndexPath:footerIndexPath] copy];
-                    CGRect fFrame = footerAttr.frame;
-                    fFrame.origin.y = [self contentHeightInSection:section] - self.footerReferenceSize.height;
-                    footerAttr.frame = fFrame;
-                    [attributesArray addObject:footerAttr];
-                    
-                    //next section header should show
-                    NSInteger nextSection = section + 1;
-                    if(nextSection < _framesArray.count){
-                        NSIndexPath *headerIndexPath = [NSIndexPath indexPathForRow:0 inSection:nextSection];
-                        UICollectionViewLayoutAttributes *headerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-                                                                                                             atIndexPath:headerIndexPath] copy];
-                        CGRect hFrame = headerAttr.frame;
-                        hFrame.origin.y = [self contentHeightInSection:section];;
-                        headerAttr.frame = hFrame;
-                        [attributesArray addObject:headerAttr];
+                    //when last cell appear
+                    if(row == currentSectionFrames.count - 1 &&
+                       currentFrame.origin.y + currentFrame.size.height + self.sectionInset.bottom <= visibleRect.origin.y + visibleRect.size.height){
+                        //current section footer should show
+                        NSIndexPath *footerIndexPath = [NSIndexPath indexPathForRow:currentSectionFrames.count inSection:section];
+                        CGFloat y = currentSectionHeight - self.footerReferenceSize.height;
+                        [attributesArray addObject:[self footerAttri:footerIndexPath withY:y]];
+                        
+                        //next section header should show
+                        NSInteger nextSection = section + 1;
+                        if(nextSection < _framesArray.count){
+                            NSIndexPath *headerIndexPath = [NSIndexPath indexPathForRow:0 inSection:nextSection];
+                            [attributesArray addObject:[self headerAttri:headerIndexPath withY:currentSectionHeight]];
+                        }
                     }
                 }
             }
@@ -141,6 +140,26 @@
 }
 
 #pragma mark---other methods
+- (UICollectionViewLayoutAttributes *)headerAttri:(NSIndexPath *)indexPath withY:(CGFloat)y
+{
+    UICollectionViewLayoutAttributes *headerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+                                                                                         atIndexPath:indexPath] copy];
+    CGRect frame = headerAttr.frame;
+    frame.origin.y = y;
+    headerAttr.frame = frame;
+    return headerAttr;
+}
+
+- (UICollectionViewLayoutAttributes *)footerAttri:(NSIndexPath *)indexPath withY:(CGFloat)y
+{
+    UICollectionViewLayoutAttributes *footerAttr = [[self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                                                                                         atIndexPath:indexPath] copy];
+    CGRect frame = footerAttr.frame;
+    frame.origin.y = y;
+    footerAttr.frame = frame;
+    return footerAttr;
+}
+
 - (void)calculateFrames
 {
     if(_framesArray.count > 0){
@@ -150,6 +169,11 @@
     
     NSInteger sectionCount = self.collectionView.numberOfSections;
     for(NSInteger section = 0; section < sectionCount; section++){
+        CGFloat preSectionHeight = 0;
+        if(section > 0){
+            preSectionHeight = [self contentHeightInSection:section - 1];
+        }
+        
         NSMutableArray *currentSectionFrames = [NSMutableArray arrayWithCapacity:sectionCount];
         [_framesArray addObject:currentSectionFrames];
         
@@ -160,7 +184,7 @@
             CGFloat y = self.headerReferenceSize.height + self.sectionInset.top + self.collectionView.contentInset.top;
             
             if(section > 0 && _framesArray.count > section - 1){
-                y = [self contentHeightInSection:section - 1] + self.headerReferenceSize.height + self.sectionInset.top;
+                y = preSectionHeight + self.headerReferenceSize.height + self.sectionInset.top;
             }
             //判断获得前一个cell的frame
             NSInteger preRow = row - 1;
@@ -193,14 +217,23 @@
     __block CGFloat height = 0;
     if(section >= 0 && _framesArray.count > section){
         NSArray *sectionYArray = [_framesArray yl_objectAtIndex:section];
-        [sectionYArray enumerateObjectsUsingBlock:^(NSValue *value, NSUInteger idx, BOOL * _Nonnull stop) {
-            CGRect frame = [value CGRectValue];
-            CGFloat y = frame.origin.y;
-            if (y > height) {
-                height = y;
+        if(sectionYArray.count == 0){
+            height = self.headerReferenceSize.height + self.sectionInset.top + self.sectionInset.bottom + self.footerReferenceSize.height;
+            if(section == 0){
+                height += self.collectionView.contentInset.top;
+            }else{
+                height += [self contentHeightInSection:section - 1];
             }
-        }];
-        height += _rowHeight + self.sectionInset.bottom + self.footerReferenceSize.height;
+        }else{
+            [sectionYArray enumerateObjectsUsingBlock:^(NSValue *value, NSUInteger idx, BOOL * _Nonnull stop) {
+                CGRect frame = [value CGRectValue];
+                CGFloat y = frame.origin.y;
+                if (y > height) {
+                    height = y;
+                }
+            }];
+            height += _rowHeight + self.sectionInset.bottom + self.footerReferenceSize.height;
+        }
     }
     return height;
 }
